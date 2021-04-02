617fe6ca3c5efa2a88f2125b11f073e68025cb35
Botov37 <55014484+Botov37@users.noreply.github.com>
2021-04-02 11:47:26 +0300

    version 1.0

---
+++ IntegralSolutionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegralSolutionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("IntegralSolutionTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ec570bf-bb56-4935-9655-b5968525a18b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IntegralSolutionTest/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using CalculateLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegralSolutionTest
+{
+    [TestClass]
+    public class ICalculateLibraryTests
+    {
+        [TestMethod]
+        public void Intergrate_xx_Gives_Correct_Result_For_Rectangle() // 1 тест для прямоугольников
+        {
+            //arrange
+            double expected = 333_333.3333;
+            double a = 0;
+            double b = 100;
+            long n = 100000;
+            RectangleCalculator rectangleCalculator = new RectangleCalculator();
+            Func<double, double> f = x => x * x;
+            //act
+            double actual = rectangleCalculator.Calculate(a, b, n, f);
+            //assert
+            Assert.AreEqual(expected, actual, 0.0001);
+
+        }
+
+        [TestMethod]
+        public void Intergrate_xx_Gives_Correct_Result_For_Trapezoid() // 2 для проверки значения трапеций
+        {
+            //arrange
+            double expected = 333_333.3333;
+            double a = 0;
+            double b = 100;
+            long n = 100000;
+            TrapCalculator trapCalculator = new TrapCalculator();
+            Func<double, double> f = x => x * x;
+            //act
+            double actual = trapCalculator.Calculate(a, b, n, f);
+            //assert
+            Assert.AreEqual(expected, actual, 0.0001);
+
+        }
+
+        [TestMethod]
+        public void Intergrate_xx_Gives_0_For_Rectangle() // 3 проверка 0 для прямоугольников 
+        {
+            //arrange
+            double expected = 0;
+            double a = 0;
+            double b = a;
+            long n = 100000;
+            RectangleCalculator rectangleCalculator = new RectangleCalculator();
+            Func<double, double> f = x => x * x;
+            //act
+            double actual = rectangleCalculator.Calculate(a, b, n, f);
+            //assert
+            Assert.AreEqual(expected, actual, 0.0001);
+        }
+
+        [TestMethod]
+        public void Intergrate_xx_Gives_0_For_Trapezoid() // 4 проверка 0 для трапеции
+        {
+            //arrange
+            double expected = 0;
+            double a = 0;
+            double b = a;
+            long n = 100000;
+            TrapCalculator trapCalculator = new TrapCalculator();
+            Func<double, double> f = x => x * x;
+            //act
+            double actual = trapCalculator.Calculate(a, b, n, f);
+            //assert
+            Assert.AreEqual(expected, actual, 0.0001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))] // 5 означает что выдаст исключение
+        public void Intergrate_xx_negative_n()
+        {
+            // Arrange
+            double a = 0;
+            double b = a;
+            long n = -10;
+            RectangleCalculator rectangleCalculator = new RectangleCalculator();
+            Func<double, double> f = x => x * x;
+            // Act
+            double actual = rectangleCalculator.Calculate(a, b, n, f);
+        }
+    }
+}
